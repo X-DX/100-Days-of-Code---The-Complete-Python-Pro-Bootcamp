c7e8cc40e1d5a4b009f715a52b82219b7359ca3d
X-DX <aruproy5637@gmail.com>
2025-01-28 08:04:17 +0530

    First Commit

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,6 +1,4 @@
-# print("My name is" + " " + "Angela")
-
-## input function take input from user
-# input('What is you Name?')
-
-print("My name is " + input(' What is you Name? ') + "!")
+print("Welcome to the Band project you will build.")
+cityname = input("What's the name of the city you grew up in?\n")
+petname = input("What's your pet's name?\n")
+print("Your band name could be: " + cityname + " "+ petname)
--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,22 @@
-len("Hello")
+print(len("Hello"))
+
+# print(len(1234)) //error
+
+# string
+print("Hello"[0])
+print("Hello"[4])
+print("Hello"[-2])
+print("123" + "456")
+
+# Integer = whole number
+print(123 + 456)
+
+# Large Number
+print(123_456_789)
+
+# Float
+print(3.123)
+
+# Boolean
+print(True)
+print(False)
