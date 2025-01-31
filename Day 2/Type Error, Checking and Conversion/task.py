d24c5548b3e63560a0d0f222d18588965362a780
X-DX <aruproy5637@gmail.com>
2025-01-31 10:28:09 +0530

    Type Conversion

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,17 @@
-len(12345)
+# The string len() function returns the length of the string
+# len(12345)
+
+# Type
+print(type("Hello"))
+print(type(123))
+print(type(1.23))
+print(type(True))
+
+# Type Conversion
+print(type(int("123")))
+print(type(int("12") + int("12")))
+print(type(float("1.22")))
+
+name_of_the_user = input("Enter your name")
+length_of_name = len(name_of_the_user)
+print("Number of letters in your name: " + str(length_of_name))
