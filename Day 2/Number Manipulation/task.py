73777652c47ff9f0967ca65c35a5ea27be3ed857
X-DX <aruproy5637@gmail.com>
2025-01-31 12:17:16 +0530

    Number Manipulation

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,16 @@
 bmi = 84 / 1.65 ** 2
+print(bmi)
+#  It just a remove the decimal
+print(int(bmi))
+
+print(round(bmi))
+print(round(bmi, 2))
+
+# Number Manipulation
+score = 0
+# User Scores a point
+score += 1
+
+# f-string
+age = 12
+print(f"I am {age} years old")
